751252ddf8bd171ebebfdc00e9519c5b49c6fca8
neo-sirapat <sirapat.asa@gmail.com>
2020-10-05 18:02:13 +0700

    calculator component imported

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <calculator id='main'></calculator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Calculator from './components/Calculator'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #main {
+    margin-top: 80px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
